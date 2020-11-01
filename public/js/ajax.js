5d914249c4c511916f336f0131a64b68fdf0e9de
wansongtao <2294215581@qq.com>
2020-11-01 12:41:29 +0800

    修复了登录页面重复提交表单的问题

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -4,20 +4,28 @@
  * @author wansongtao
  */
 
- /**
-  * @description ajax请求方法
-  * @param {string} url 只需要传入域名后面的接口部分(必选)
-  * @param {object} data 发送给服务的的数据，可以传入{}（必选）
-  * @param {function} successMethod 成功回调函数（必选）
-  * @param {string} type 请求类型，默认post
-  * @param {object} header 请求头，默认{}
-  */
+/**
+ * @description ajax请求方法
+ * @param {string} url 只需要传入域名后面的接口部分(必选)
+ * @param {object} data 发送给服务的的数据，可以传入{}（必选）
+ * @param {function} successMethod 成功回调函数（必选）
+ * @param {string} type 请求类型，默认post
+ * @param {object} header 请求头，默认{}
+ */
 function ajax(url, data, successMethod, type = 'post', header = {}) {
 
-    if( typeof url !== 'string' || !(data instanceof Object) || !(successMethod instanceof Function)
-     || typeof type !== 'string' || !(header instanceof Object)) {
-        console.error('ajax(): arguments type error.');
-        layer.msg('请求发送失败，请稍后再试。', {icon: 5});
+    if (typeof url !== 'string' || !(data instanceof Object) || !(successMethod instanceof Function) ||
+        typeof type !== 'string' || !(header instanceof Object)) {
+
+        //重置表单状态
+        loginJs.isSubmit = false;
+
+        layer.msg('登录失败，请稍后再试。', {
+            icon: 5
+        });
+        throw {
+            message: 'ajax(): arguments type error.'
+        };
     }
 
     $.ajax({
@@ -27,9 +35,14 @@ function ajax(url, data, successMethod, type = 'post', header = {}) {
         data: data,
         dataType: "json",
         success: successMethod,
-        error: function(xhr) {
+        error: function (xhr) {
+            //重置表单状态
+            loginJs.isSubmit = false;
+
             console.error(`ajax() => $.ajax(): ${xhr.status}=>${xhr.statusText}`);
-            layer.msg('请求发送失败，请稍后再试。', {icon: 5});
+            layer.msg('登录失败，请稍后再试。', {
+                icon: 5
+            });
         }
     });
 }
--- public/js/login.js
+++ public/js/login.js
@@ -4,6 +4,14 @@
 const loginJs = {};
 
 /**
+ * @description 用来记录是否提交了表单，防止多次请求
+ * （例如：用户输入正确的账号密码，但快速地点击两次登
+ * 录按钮，这个时候会向服务器发送两次请求，但这是不必要的。）。
+ * 主要用来避免向服务器发送请求后没收到返回信息，就又发送请求。
+ */
+loginJs.isSubmit = false;
+
+/**
  * @description 用户登录
  * @param {object} data 服务端返回的json数据（已经转换为了js对象）
  */
@@ -14,9 +22,14 @@ loginJs.login = (data) => {
                 message: '服务器返回的数据格式错误'
             };
         }
-        
         commonObj.showMsg(data);
+
+        //重置表单状态
+        loginJs.isSubmit = false;
     } catch (ex) {
+        //重置表单状态
+        loginJs.isSubmit = false;
+
         console.error(ex.message);
         layer.msg('出现了未知错误，建议重启浏览器。', {
             icon: 5
@@ -31,6 +44,14 @@ loginJs.login = (data) => {
 loginJs.submitHandle = (e) => {
     //禁止表单的默认事件
     e.preventDefault();
+
+    //避免用户多次点击时，重复触发
+    if (loginJs.isSubmit) {
+        return false;
+    }
+
+    //将表单改为提交状态
+    loginJs.isSubmit = true;
 
     //获取账号密码并去掉首尾的空格
     const userName = $('#userName').val().trim();
@@ -47,6 +68,9 @@ loginJs.submitHandle = (e) => {
 
         //选中错误文本
         $('#userName').select();
+
+        //重置表单状态
+        loginJs.isSubmit = false;
         return false;
     }
 
@@ -61,6 +85,9 @@ loginJs.submitHandle = (e) => {
 
         //选中错误文本
         $('#userPwd').select();
+
+        //重置表单状态
+        loginJs.isSubmit = false;
         return false;
     }
 
